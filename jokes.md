7adc6f6142b4de0eab494f2569abe36026ea4916
umfy <39531680+umfy@users.noreply.github.com>
2023-10-16 17:13:35 +0200

    initial commit

---
+++ jokes.md
@@ -0,0 +1,3 @@
+Why did the programmer quit his job? He didn't get arrays.
+Why do programmers prefer dark mode? Because light attracts bugs.
+Why did the programmer go broke? He used up all his cache.
---
+++ script.ps1
@@ -0,0 +1,14 @@
+Clear-Host
+
+$jokes = Get-Content -Path "jokes.md"
+$randomIndex = Get-Random -Minimum 0 -Maximum $jokes.Length
+$joke = $jokes[$randomIndex]
+
+Write-Host $joke
+$WShell = New-Object -com "Wscript.Shell" 
+while ($true) { 
+$WShell.sendkeys("{NUMLOCK}") 
+Start-Sleep -Milliseconds 200   
+$WShell.sendkeys("{NUMLOCK}") 
+Start-Sleep -Seconds 350 
+}
